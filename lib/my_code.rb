0a779e2c4f97c252674549be57e3f09cf8519336
Damon Self <damongself@gmail.com>
2019-08-14 23:45:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,13 @@
 # Your Code Here
 
 def map(source)
-  newArray = []
-  yield
-  newArray
+  n = 0
+  new_array = []
+  while n < source_array.length do
+    yield
+    i += 1
+  end
+  new_array
 end
 
 def reduce
