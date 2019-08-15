a3d72102c7e75c8909dfad3f8a9e00c5da59d96a
Damon Self <damongself@gmail.com>
2019-08-15 00:24:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ end
 def reduce(source_array, memo = 0)
   i = 0
   while i < source_array.length do
-    memo += yield(source_array[i])
+    memo = yield(source_array[i])
     i += 1
   end
   total
