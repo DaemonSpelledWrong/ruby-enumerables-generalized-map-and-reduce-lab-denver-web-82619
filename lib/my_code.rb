3ebecac1bb628c4e4dd237a95013c1766bdfbb4e
Damon Self <damongself@gmail.com>
2019-08-15 00:07:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,15 +13,9 @@ end
 def reduce(source_array, memo = 0)
   i = 0
   total = memo
-    while i < source_array.length do
-    if source_array[i]
-     return total = true
-    elsif total == source_array.length
-      return true
-    else
-      total = false
-      i += 1
-    end
+  while i < source_array.length do
+    total = total + source_array[i]
+    i += 1
   end
   total
 end
