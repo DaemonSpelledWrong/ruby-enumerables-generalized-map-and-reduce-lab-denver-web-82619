07ae1ba06460e25ae923c2415dcffbb844d680a9
Damon Self <damongself@gmail.com>
2019-08-15 00:02:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,9 +10,9 @@ def map(source_array)
   new_array
 end
 
-def reduce(source_array, starting_point = 0)
+def reduce(source_array, memo = 0)
   i = 0
-  total = starting_point
+  total = memo
   
   total
 end
